ba00394ea4aa0fc6a5a275e0066edaaf67d8aeb4
UV05BQ <yvangomezalonso@gmail.com>
2020-10-06 13:42:02 +0200

    fix: update example to use html template instead of doing it with code

--- WebComponentsAPI/3-custom-elements-with-slots.js
+++ WebComponentsAPI/3-custom-elements-with-slots.js
@@ -2,41 +2,11 @@ class MyCustomElementWithSlots extends HTMLElement {
     constructor() {
         super();
 
-        const p = document.createElement('p');
-        p.innerText = `This is part of the component, and bellow we are using a slot where you can add any element from outside`;
-        p.classList = ['my-class'];
-
-        const style = document.createElement('style');
-        style.textContent = `
-            .my-class {
-                background: black;
-                color: white;
-                min-height: 70px;
-                border-radius: 10px;
-                text-align: center;
-                font-family: 'Roboto', sans-serif;
-            }
-        `;
-
-        // Create a slot with a name
-        const slot = document.createElement('slot');
-        slot.setAttribute('name', 'my-slot-name');
-
+        const myCustomElementTemplate = document.getElementById('my-custom-element-with-slots');
+        const cloned = myCustomElementTemplate.content.cloneNode(true);
         const shadow = this.attachShadow({mode: 'open'});
 
-        shadow.appendChild(style);
-        shadow.appendChild(p);
-
-        // And just attach it to the shadowRoot
-        shadow.appendChild(slot);
-
-
-        // We can use a default slot, but only one!
-        // const defaultSlot = document.createElement('slot');
-        // const defaultSlot2 = document.createElement('slot');
-
-        // shadow.appendChild(defaultSlot);
-        // shadow.appendChild(defaultSlot2);
+        shadow.appendChild(cloned);
     }
 }
 
